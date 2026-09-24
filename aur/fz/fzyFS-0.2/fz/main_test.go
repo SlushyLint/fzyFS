6f07d9bd485a84f9d8de688a68223823191e89f9
SlushyLint <lewislintlicker+fuckyougithub@googlemail.com>
2026-09-24 13:38:27 +0000

    AUR support (Not in repos yet)

--- aur/fz/fzyFS-0.2/fz/main_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "testing"
-
-func TestRunWithoutQueryReturnsUsage(t *testing.T) {
-	if got := run([]string{}); got != 1 {
-		t.Fatalf("run([]string{}) = %d, want 1", got)
-	}
-}
